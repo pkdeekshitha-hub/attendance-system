5b6bbe34fe0c265b74be3a60a9d61af837315956
Deekshitha <pkdeekshitha2006@gmail.com>
2026-06-11 19:25:38 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,13 @@
+import os
+from urllib.parse import urlparse
+
+url = urlparse(os.environ.get('MYSQL_PUBLIC_URL', ''))
+
 class Config:
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'svt1713'
-    MYSQL_DB = 'attendance_db'
-    JWT_SECRET_KEY = 'supersecretkey123'
+    MYSQL_HOST = url.hostname
+    MYSQL_USER = url.username
+    MYSQL_PASSWORD = url.password
+    MYSQL_DB = url.path.lstrip('/')
+    MYSQL_PORT = url.port or 3306
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'supersecretkey123')
     JWT_ACCESS_TOKEN_EXPIRES = False

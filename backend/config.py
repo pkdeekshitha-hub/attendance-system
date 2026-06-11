91942e8cc8bc77052750e8fa173e96a561a54565
P K Deekshitha <pkdeekshitha2006@gmail.com>
2026-06-11 19:27:33 +0530

    fix: use MYSQL_PUBLIC_URL for Railway connection

--- backend/config.py
+++ backend/config.py
@@ -1,9 +1,13 @@
 import os
+from urllib.parse import urlparse
+
+url = urlparse(os.environ.get('MYSQL_PUBLIC_URL', ''))
 
 class Config:
-    MYSQL_HOST = os.environ.get('MYSQL_HOST', 'localhost')
-    MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
-    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', '')
-    MYSQL_DB = os.environ.get('MYSQL_DB', 'attendance_db')
+    MYSQL_HOST = url.hostname
+    MYSQL_USER = url.username
+    MYSQL_PASSWORD = url.password
+    MYSQL_DB = url.path.lstrip('/')
+    MYSQL_PORT = url.port or 3306
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'supersecretkey123')
     JWT_ACCESS_TOKEN_EXPIRES = False
